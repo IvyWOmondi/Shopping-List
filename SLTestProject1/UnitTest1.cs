1ca43298f7687238d8c2821d08e90eee00285f0f
Ivy Wanjiku Omondi <Omondi581699@newmanu.edu>
2025-11-28 20:55:01 -0600

    Uncommitted changes saved to new experimental branch.

---
+++ SLTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿namespace SLTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ShouldAddItem()
+        {
+            var add = new AddToListItem()
+            {
+                Items =
+                {
+                    "milk",
+                    "bread",
+                    "eggs",
+                    "juice",
+                }
+            };
+
+        }
+
+        [Test]
+        public void ShouldRemoveItem()
+        {
+            var remove = new RemoveFromListItem();
+            remove.Items.Remove("milk");
+            remove.Items.Remove("bread");
+            remove.Items.Remove("eggs");
+            remove.Items.Remove("juice");
+            remove.Item = "eggs";
+            //Assert.That(remove.Items.Contains("eggs"), Is.False);
+        }
+    }
+}
